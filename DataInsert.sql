157c5bacd98f88153100b3153a0e64a5295731ac
Cangerha <1796932364@qq.com>
2026-05-13 18:02:34 +0800

    Add data insert script. Modify constraint of some of the fields.

---
+++ DataInsert.sql
@@ -0,0 +1,114 @@
+-- Active: 1775733622997@@127.0.0.1@3306@experiment
+
+-- 为customers表插入20行数据
+CREATE PROCEDURE InsertCustomers()
+BEGIN
+    DECLARE i INT;
+    SET i = 1;
+    WHILE i <= 20 DO
+        INSERT INTO customers (c_id,c_name,c_phone,c_address) 
+        VALUES (
+            CONCAT('100000',LPAD(i, 2, '0')),
+            CONCAT('用户',LPAD(i, 2, '0')),
+            CONCAT('136000011', LPAD(i, 2, '0')), 
+            CONCAT('海礼1', LPAD(i, 2, '0'))
+        );
+        SET i = i + 1;
+    END WHILE;
+END
+
+CALL InsertCustomers ();
+
+-- 为shops表插入20行数据
+CREATE PROCEDURE InsertShops()
+BEGIN
+    DECLARE i INT;
+    SET i = 1;
+    WHILE i <= 20 DO
+        INSERT INTO shops (s_id,s_name,s_phone,s_address)
+        VALUES (
+            CONCAT('100000',LPAD(i, 2, '0')),
+            CONCAT('商家',LPAD(i, 2, '0')),
+            CONCAT('158000010', LPAD(i, 2, '0')), 
+            CONCAT('海大路', LPAD(i, 2, '0'),'号')
+        );
+        SET i = i + 1;
+    END WHILE;
+END
+
+CALL InsertShops ();
+
+-- 为foods表插入20行数据
+CREATE PROCEDURE InsertFoods()
+BEGIN
+    DECLARE i INT;
+    SET i = 1;
+    WHILE i <= 20 DO
+        INSERT INTO foods (f_id,f_name,f_price,s_id)
+        VALUES (
+            LPAD(i, 2, '0'),
+            CONCAT('菜品',LPAD(i, 2, '0')),
+            ROUND(RAND() * (50 - 10) + 10), 
+            CONCAT('100000',LPAD(ROUND(RAND() * 19 + 1), 2, '0'))
+        );
+        SET i = i + 1;
+    END WHILE;
+END
+
+CALL InsertFoods ();
+
+-- 为orders表插入20行的id，客户id，与商户id
+CREATE PROCEDURE InsertOrders()
+BEGIN
+    DECLARE i INT;
+    SET i = 1;
+    WHILE i <= 20 DO
+        INSERT INTO orders (o_id,c_id,s_id)
+        VALUES (
+            LPAD(i, 2, '0'),
+            CONCAT('100000',LPAD(ROUND(RAND() * 19 + 1), 2, '0')),
+            CONCAT('100000',LPAD(ROUND(RAND() * 19 + 1), 2, '0'))
+        );
+        SET i = i + 1;
+    END WHILE;
+END
+
+CALL InsertOrders ();
+
+-- 为order_details表插入20行数据
+CREATE PROCEDURE InsertOrderDetails()
+BEGIN
+    DECLARE i INT;
+    DECLARE food_id INT;
+    DECLARE quantity INT;
+
+    SET i = 1;
+    SET food_id=0;
+    SET quantity=0;
+
+    WHILE i <= 20 DO
+        SET food_id=ROUND(RAND() * 19 + 1);
+        SET quantity=ROUND(RAND() * 2 + 1);
+        INSERT INTO order_details (od_id,o_id,f_id,od_quantity,od_subtotal)
+        VALUES (
+            i,
+            i,
+            food_id,
+            quantity,
+            quantity*(SELECT f_price FROM foods WHERE f_id=food_id)
+        );
+        SET i = i + 1;
+    END WHILE;
+END
+
+CALL InsertOrderDetails ();
+
+-- 根据生成的订单明细计算订单总金额
+UPDATE orders
+SET
+    o_sum = (
+        SELECT SUM(od_subtotal)
+        FROM order_details
+        WHERE
+            order_details.o_id = orders.o_id
+    )
--- TableCreation.sql
+++ TableCreation.sql
@@ -3,8 +3,8 @@
 -- 1. 客户表
 CREATE TABLE customers (
     c_id INT PRIMARY KEY AUTO_INCREMENT COMMENT '客户ID',
-    c_name VARCHAR(50) NOT NULL COMMENT '客户姓名',
-    c_phone VARCHAR(20) NOT NULL COMMENT '客户电话',
+    c_name VARCHAR(50) COMMENT '客户姓名',
+    c_phone VARCHAR(20) COMMENT '客户电话',
     c_address VARCHAR(200) COMMENT '地址',
     c_total_spent DECIMAL(10, 2) DEFAULT 0.00 COMMENT '累积消费'
 ) COMMENT '客户表';
@@ -23,7 +23,7 @@ CREATE TABLE foods (
     f_name VARCHAR(100) NOT NULL COMMENT '菜品名称',
     f_price DECIMAL(10, 2) NOT NULL COMMENT '单价',
     s_id INT NOT NULL COMMENT '所属商家ID',
-    CONSTRAINT foods_fk_s_id FOREIGN KEY (s_id) REFERENCES shops (s_id) ON DELETE RESTRICT
+    CONSTRAINT foods_fk_s_id FOREIGN KEY (s_id) REFERENCES shops (s_id) ON DELETE CASCADE
 ) COMMENT '菜品表';
 
 -- 4. 订单表
@@ -33,8 +33,8 @@ CREATE TABLE orders (
     o_sum DECIMAL(10, 2) NOT NULL DEFAULT 0.00 COMMENT '订单总金额',
     c_id INT NOT NULL COMMENT '客户ID',
     s_id INT NOT NULL COMMENT '商家ID',
-    CONSTRAINT orders_fk_c_id FOREIGN KEY (c_id) REFERENCES customers (c_id) ON DELETE RESTRICT,
-    CONSTRAINT orders_fk_s_id FOREIGN KEY (s_id) REFERENCES shops (s_id) ON DELETE RESTRICT
+    CONSTRAINT orders_fk_c_id FOREIGN KEY (c_id) REFERENCES customers (c_id) ON DELETE CASCADE,
+    CONSTRAINT orders_fk_s_id FOREIGN KEY (s_id) REFERENCES shops (s_id) ON DELETE CASCADE
 ) COMMENT '订单表';
 
 -- 5. 订单明细表
@@ -42,8 +42,8 @@ CREATE TABLE order_details (
     od_id INT PRIMARY KEY AUTO_INCREMENT COMMENT '明细ID',
     o_id INT NOT NULL COMMENT '订单ID',
     f_id INT NOT NULL COMMENT '菜品ID',
-    f_quantity INT NOT NULL CHECK (f_quantity > 0) COMMENT '数量',
+    od_quantity INT NOT NULL CHECK (od_quantity > 0) COMMENT '数量',
     od_subtotal DECIMAL(10, 2) NOT NULL COMMENT '小计金额',
     CONSTRAINT order_details_fk_o_id FOREIGN KEY (o_id) REFERENCES orders (o_id) ON DELETE CASCADE,
-    CONSTRAINT order_details_fk_f_id FOREIGN KEY (f_id) REFERENCES foods (f_id) ON DELETE RESTRICT
+    CONSTRAINT order_details_fk_f_id FOREIGN KEY (f_id) REFERENCES foods (f_id) ON DELETE CASCADE
 ) COMMENT '订单明细表';
